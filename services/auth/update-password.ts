ebd83eabb3c8c5a8635c08160d00b8bc55dc7b23
Layss kheir <laysskheir@gmail.com>
2025-04-14 18:26:40 +0300

    fix prisma in production

--- services/auth/update-password.ts
+++
@@ -1,5 +0,0 @@
-// await user.changePassword({
-//     newPassword: "newPassword123",
-//     currentPassword: "oldPassword123",
-//     revokeOtherSessions: true, // revoke all other sessions the user is signed into
-// });
